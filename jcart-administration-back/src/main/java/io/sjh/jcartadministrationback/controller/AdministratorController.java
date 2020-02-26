953e51bf74c72b99e1b294d18d1691504b8196a1
SJH2000628 <1134826337@qq.com>
2020-02-26 22:43:44 +0800

    dralt some api

---
+++ jcart-administration-back/src/main/java/io/sjh/jcartadministrationback/controller/AdministratorController.java
@@ -0,0 +1,28 @@
+package io.sjh.jcartadministrationback.controller;
+
+import io.sjh.jcartadministrationback.dto.in.AdministratorLoginInDTO;
+import io.sjh.jcartadministrationback.dto.in.AdministratorUpdateProfileInDTO;
+import io.sjh.jcartadministrationback.dto.out.AdministratorGetProfileOutDTO;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/administrator")
+public class AdministratorController {
+    @GetMapping("/login")
+    public String login(AdministratorLoginInDTO administratorLoginInDTO){
+        return null;
+    }
+
+
+
+    @GetMapping("/getProfile")
+    public AdministratorGetProfileOutDTO getProfile(Integer adminstratorId){
+
+        return null;
+    }
+
+    @PostMapping("/updateProdfile")
+    public void updateProdfile(@RequestBody AdministratorUpdateProfileInDTO administratorUpdateProfileInDTO){
+
+    }
+}
