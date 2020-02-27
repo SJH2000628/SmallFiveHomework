2173a3253f6ad2038b678c5532a3073ac20e13b9
SJH2000628 <1134826337@qq.com>
2020-02-27 15:43:17 +0800

    add product search api

---
+++ jcart-administration-back/src/main/java/io/sjh/jcartadministrationback/controller/ProductController.java
@@ -0,0 +1,19 @@
+package io.sjh.jcartadministrationback.controller;
+
+import io.sjh.jcartadministrationback.dto.in.ProductSearchInDTO;
+import io.sjh.jcartadministrationback.dto.out.PageOutDTO;
+import io.sjh.jcartadministrationback.dto.out.ProductListOutDTO;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/product")
+public class ProductController {
+    @GetMapping("/search")
+    public PageOutDTO<ProductListOutDTO> search(ProductSearchInDTO productSearchInDTO,
+                                                @RequestParam Integer pageName){
+        return null;
+    }
+}
