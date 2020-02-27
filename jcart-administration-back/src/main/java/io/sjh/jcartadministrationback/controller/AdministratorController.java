9708b0fbf981f794ecd32f4c23b7bf8b10ef4006
SJH2000628 <1134826337@qq.com>
2020-02-27 15:20:36 +0800

    add some administrator apis

--- jcart-administration-back/src/main/java/io/sjh/jcartadministrationback/controller/AdministratorController.java
+++ jcart-administration-back/src/main/java/io/sjh/jcartadministrationback/controller/AdministratorController.java
@@ -1,8 +1,11 @@
 package io.sjh.jcartadministrationback.controller;
 
 import io.sjh.jcartadministrationback.dto.in.AdministratorLoginInDTO;
+import io.sjh.jcartadministrationback.dto.in.AdministratorRestPwdInDTO;
 import io.sjh.jcartadministrationback.dto.in.AdministratorUpdateProfileInDTO;
 import io.sjh.jcartadministrationback.dto.out.AdministratorGetProfileOutDTO;
+import io.sjh.jcartadministrationback.dto.out.AdministratorListOutDTO;
+import io.sjh.jcartadministrationback.dto.out.PageOutDTO;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -25,5 +28,21 @@ public class AdministratorController {
     @PostMapping("/updateProdfile")
     public void updateProdfile(@RequestBody AdministratorUpdateProfileInDTO administratorUpdateProfileInDTO){
 
+     }
+     /*
+     *獲取重置嗎
+     *  */
+     @GetMapping("/getPwdResetCode")
+    public String getPwdResetCode(@RequestParam String email){
+        return null;
+     }
+     @PostMapping("/resetPwd")
+     public void resetPwd(@RequestBody AdministratorRestPwdInDTO administratorRestPwdInDTO){
+
+     }
+
+     @GetMapping("/getlist")
+    public PageOutDTO<AdministratorListOutDTO> getlist(@RequestParam Integer pageNum){
+        return null;
      }
 }
