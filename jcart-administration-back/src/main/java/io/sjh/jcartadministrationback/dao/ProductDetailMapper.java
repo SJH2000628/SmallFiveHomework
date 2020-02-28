0796beaab9374d49a1dd57e3cf19e7205addc32d
SJH2000628 <1134826337@qq.com>
2020-02-28 14:36:36 +0800

    add mybatis files

---
+++ jcart-administration-back/src/main/java/io/sjh/jcartadministrationback/dao/ProductDetailMapper.java
@@ -0,0 +1,19 @@
+package io.sjh.jcartadministrationback.dao;
+
+import io.sjh.jcartadministrationback.po.ProductDetail;
+
+public interface ProductDetailMapper {
+    int deleteByPrimaryKey(Integer productId);
+
+    int insert(ProductDetail record);
+
+    int insertSelective(ProductDetail record);
+
+    ProductDetail selectByPrimaryKey(Integer productId);
+
+    int updateByPrimaryKeySelective(ProductDetail record);
+
+    int updateByPrimaryKeyWithBLOBs(ProductDetail record);
+
+    int updateByPrimaryKey(ProductDetail record);
+}
