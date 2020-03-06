5ef895c6c60d04ba4fc91d10487088679b9cad04
SJH2000628 <1134826337@qq.com>
2020-03-06 16:22:32 +0800

    add administration customer set status page

--- jcart-administration-front/viewmodels/customer-search.js
+++ jcart-administration-front/viewmodels/customer-search.js
@@ -2,7 +2,12 @@ var app = new Vue({
     el: '#app',
     data: {
         pageInfo:'',
-        pageNum:1
+        pageNum:1,
+        statuses:[
+            { value: 0, label: '禁用' },
+            { value: 1, label: '启用' },
+            { value: 2, label: '不安全' }
+        ]
     },
     mounted(){
         console.log('view mounted')
@@ -13,6 +18,10 @@ var app = new Vue({
           console.log('page change');
           this.pageNum = val;
           this.searchCustomer();  
+        },
+        handleUpdateStatus(index,row){
+            console.log('update status click');
+            this.updateCustomerStatus(row.customerId,row.status);
         },
         searchCustomer(){
             axios.get('/customer/search', {
@@ -27,6 +36,19 @@ var app = new Vue({
                 .catch(function (error) {
                     console.log(error);
                 })
+        },
+        updateCustomerStatus(customerId,status){
+            axios.post('/customer/setStatus', {
+                customerId: customerId,
+                status: status
+            })
+                .then(function (response) {
+                    console.log(response);
+                    alert('状态更新成功');
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
         }
     }
 })
