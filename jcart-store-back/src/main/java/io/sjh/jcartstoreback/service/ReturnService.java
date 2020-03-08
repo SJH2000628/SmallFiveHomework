4084b98ceabe6dd0486423e51f889ffd17886f55
SJH2000628 <1134826337@qq.com>
2020-03-08 17:34:12 +0800

    add store return get list api

--- jcart-store-back/src/main/java/io/sjh/jcartstoreback/service/ReturnService.java
+++ jcart-store-back/src/main/java/io/sjh/jcartstoreback/service/ReturnService.java
@@ -1,9 +1,10 @@
 package io.sjh.jcartstoreback.service;
 
+import com.github.pagehelper.Page;
 import io.sjh.jcartstoreback.po.Return;
 
 public interface ReturnService {
     Integer create(Return aReturn);
 
-
+    Page<Return> getPageCustomerId(Integer customerId,Integer pageNum);
 }
--- jcart-store-back/src/main/java/io/sjh/jcartstoreback/service/impl/ReturnServiceImpl.java
+++ jcart-store-back/src/main/java/io/sjh/jcartstoreback/service/impl/ReturnServiceImpl.java
@@ -1,5 +1,7 @@
 package io.sjh.jcartstoreback.service.impl;
 
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
 import io.sjh.jcartstoreback.dao.ReturnMapper;
 import io.sjh.jcartstoreback.po.Return;
 import io.sjh.jcartstoreback.service.ReturnService;
@@ -16,5 +18,12 @@ public class ReturnServiceImpl implements ReturnService {
         Integer returnId = aReturn.getReturnId();
         return returnId;
 
+    }
+
+    @Override
+    public Page<Return> getPageCustomerId(Integer customerId, Integer pageNum) {
+        PageHelper.startPage(pageNum,10);
+        Page<Return> page = returnMapper.selectPageByCustomerId(customerId);
+        return page;
     }
 }
