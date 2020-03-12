d3a91a5d3e0c038a6fec010e456f6d2ea0778b00
SJH2000628 <1134826337@qq.com>
2020-03-12 23:44:23 +0800

    add administration return search fuzzy search paage

--- jcart-administration-front/viewmodels/return-search.js
+++ jcart-administration-front/viewmodels/return-search.js
@@ -2,13 +2,45 @@ var app = new Vue({
     el: '#app',
     data: {
         pageInfo: '',
-        pageNum: 1
+        pageNum: 1,
+        returnId:'',
+        orderId: '',
+        customerName: '',
+        productCode: '',
+        productName: '',
+        startTimestamp: '',
+        endTimestamp: '',
+        selectedStatus:'',
+        data:'',
+        statuses:[
+            { value: 0, label: '待处理' },
+            { value: 1, label: '待取货' },
+            { value: 2, label: '正在处理' },
+            { value: 3, label: '完成' },
+            { value: 4, label: '拒绝' }
+        ]
     },
     mounted() {
         console.log('view mounted');
         this.searchReturn();
     },
     methods: {
+        handleFuzzyClick() {
+            console.log('fuzzy click')
+            this.pageNum = 1;
+            this.searchReturn();
+        },
+        handleClearClick() {
+            console.log('clear click')
+            this.returnId = '';
+            this.orderId = '';
+            this.customerName = '';
+            this.productCode = '';
+            this.productName = '';
+            this.startTimestamp = '';
+            this.endTimestamp = '';
+            this.selectedStatus = '';
+        },
         handlePageChange(val) {
             console.log('page changed', val);
             this.pageNum = val;
@@ -17,7 +49,17 @@ var app = new Vue({
         searchReturn() {
             axios.get('/return/search', {
                 params: {
+                    
+                    returnId: this.returnId,
+                    orderId: this.orderId,
+                    customerName: this.customerName,
+                    productCode: this.productCode,
+                    productName: this.productName,
+                    startTimestamp: this.startTimestamp ? this.startTimestamp.getTime() : '',
+                    endTimestamp: this.endTimestamp ? this.endTimestamp.getTime() : '',
+                    status: this.selectedStatus,
                     pageNum: this.pageNum
+                    
                 }
             })
                 .then(function (response) {
