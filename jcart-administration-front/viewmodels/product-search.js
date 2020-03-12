7521cc307a725a31fbb20d13da4772d4874812d7
SJH2000628 <1134826337@qq.com>
2020-03-12 18:26:29 +0800

    add administration product search fuzzy search page

--- jcart-administration-front/viewmodels/product-search.js
+++ jcart-administration-front/viewmodels/product-search.js
@@ -2,13 +2,36 @@ var app = new Vue({
     el: '#app',
     data: {
         pageInfo:'',
-        pageNum:1
+        pageNum:1,
+        productCode:'',
+        ProductName: '',
+        price: '',
+        stockQuantity: '',
+        selectedStatus: '',
+        statuses:[
+            { value: 0, label: '下架' },
+            { value: 1, label: '上架' },
+            { value: 2, label: '待审核' }
+        ]
     },
     mounted(){
         console.log('view mounted');
         this.searchProduct();
     },
     methods:{
+        handleFuzzyClick(){
+            console.log('fuzzy click')
+            this.pageNum = 1;
+            this.searchProduct();
+        },
+        handleClearClick(){
+            console.log('clear click')
+            this.productCode = '';
+            this.ProductName = '';
+            this.price = '';
+            this.stockQuantity = '';
+            this.selectedStatus = '';
+        },
         handlePageChange(val){
             console.log('page change');
             this.pageNum = val;
@@ -17,7 +40,13 @@ var app = new Vue({
         searchProduct(){
             axios.get('/product/search', {
                 params: {
+                    productCode: this.productCode,
+                    ProductName: this.ProductName,
+                    price: this.price,
+                    stockQuantity: this.stockQuantity,
+                    status: this.selectedStatus,
                     pageNum: this.pageNum
+
                 }
             })
                 .then(function (response) {
