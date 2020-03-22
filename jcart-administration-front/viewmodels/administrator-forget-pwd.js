0f08c032b51eebc7a98dd886dc99020b2768d523
SJH2000628 <1134826337@qq.com>
2020-03-22 16:50:16 +0800

    add administration page setInterval

--- jcart-administration-front/viewmodels/administrator-forget-pwd.js
+++ jcart-administration-front/viewmodels/administrator-forget-pwd.js
@@ -2,12 +2,23 @@ var app = new Vue({
     el: '#app',
     data: {
         email: '',
-        loading:false
+        loading:false,
+        buttonEnable:true,
+        counter:60
     },
     methods: {
         handleFindBackPwdClick() {
             console.log('find back pwd click');
             this.loading=true;
+            this.buttonEnable = false;
+            this.counter = 60;
+            setInterval(function () {
+                console.log('count down')
+                app.counter--;
+                if(app.counter<0){
+                    app.buttonEnable = true;
+                }
+            }, 1000);
             this.getPwdResetCode();
         },
         getPwdResetCode() {
@@ -19,7 +30,7 @@ var app = new Vue({
                 .then(function (response) {
                     console.log(response);
                     app.loading = false;
-                    alert('重置码已发送到邮箱');
+                    // alert('重置码已发送到邮箱');
                 })
                 .catch(function (error) {
                     console.log(error);
