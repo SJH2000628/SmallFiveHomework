2774a753f4c5002dcb5a4b27820096cb28c11c70
SJH2000628 <1134826337@qq.com>
2020-03-23 17:30:15 +0800

    first init support service

---
+++ jcart-support-back/src/test/java/io/sjh/jcartsupportback/JcartSupportBackApplicationTests.java
@@ -0,0 +1,13 @@
+package io.sjh.jcartsupportback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JcartSupportBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
